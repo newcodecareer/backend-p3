bd39ff7424b0e172d7c37ade89ca19c875edaf37
icegerb <jonreb.k@hotmail.com>
2022-09-25 18:15:16 +1000

    feat: add create new customer

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const userRouter = require('./user');
+const customerRouter = require('./customer');
 
 const v1Router = express.Router();
 
 v1Router.use('/users', userRouter);
+v1Router.use('/customers', customerRouter);
 
 module.exports = v1Router;
