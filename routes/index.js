38dc5bea6e805d0731b269f1467d1752bd3f4db9
newcodecareer <815773677@qq.com>
2022-09-28 12:49:51 +1000

    feat: creat comment and schema

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const userRouter = require('./user');
 const customerRouter = require('./customer');
 
+const commentRouter = require('./comment');
+
 const v1Router = express.Router();
 
 v1Router.use('/users', userRouter);
 v1Router.use('/customers', customerRouter);
+
+
+v1Router.use('/comment', commentRouter);
 
 module.exports = v1Router;
