1bfe2c4335c9a681301f8a0c114c8f89cf2c8ad2
Erica Peng <ericapeng451@gmail.com>
2022-09-29 21:57:55 +1000

    FCT-24 <PATCH - update skilllist>

--- controllers/skilllist.js
+++ controllers/skilllist.js
@@ -1,26 +1,65 @@
 const { StatusCodes } = require('http-status-codes');
 const Skilllist = require('../models/Skilllist');
 
+//POST - create new skill list
+
 const createNewSkilllist = async (req, res) => {
-    const { Listcategory, Listtitle, Listdescription, Listprice, Listdistance, Listavaliability } = req.body;
+  const { Listcategory, Listtitle, Listdescription, Listprice, Listdistance, Listavaliability } = req.body;
     
-    try {
-      const NewSkilllist = await Skilllist({
-        Listcategory, 
+  try {
+    const NewSkilllist = await Skilllist({
+      Listcategory, 
+      Listtitle, 
+      Listdescription, 
+      Listprice, 
+      Listdistance, 
+      Listavaliability
+  }).save();
+
+  return res.status(StatusCodes.OK).json(NewSkilllist);
+  } catch (err) {
+      return res.status(StatusCodes.NOT_IMPLEMENTED).json(err);
+  }
+};
+
+//PATCH - Update skill list
+
+const updateSkilllistById = async (req, res) => {
+  const { id } = req.params;
+  const { 
+    Listcategory, 
+    Listtitle, 
+    Listdescription, 
+    Listprice, 
+    Listdistance, 
+    Listavaliability 
+  } = req.body;
+
+  try {
+    const updatedSkilllist = await Skilllist.findOneAndUpdate(
+      { _id: id },
+      { Listcategory, 
         Listtitle, 
         Listdescription, 
         Listprice, 
         Listdistance, 
-        Listavaliability
-    }).save();
+        Listavaliability  
+      },
+      { new: true }
+    );
 
-      return res.status(StatusCodes.OK).json(NewSkilllist);
-    } catch (err) {
-      return res.status(StatusCodes.NOT_IMPLEMENTED).json(err);
-    }
-  };
+    return res.status(StatusCodes.OK).json(updatedSkilllist);
+  } catch (err) {
+    return res.status(StatusCodes.NOT_FOUND).json(err);
+  }
+};
+
+
+
+
   
-  module.exports = {
-    createNewSkilllist,
-  };
+module.exports = {
+  createNewSkilllist,
+  updateSkilllistById
+};
   
--- routes/skilllist.js
+++ routes/skilllist.js
@@ -1,8 +1,13 @@
 const { Router } = require('express');
-const { createNewSkilllist } = require('../controllers/skilllist');
+
+const { 
+    createNewSkilllist,
+    updateSkilllistById,
+} = require('../controllers/skilllist');
 
 const skilllistRouter = Router();
 
 skilllistRouter.post('/', createNewSkilllist);
+skilllistRouter.patch('/:id', updateSkilllistById);
 
 module.exports = skilllistRouter;
