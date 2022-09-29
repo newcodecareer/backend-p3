5cf53d715c25f6efabd8aec724935375f058d585
newcodecareer <815773677@qq.com>
2022-09-29 10:07:59 +1000

    feat: delete comments by id

--- routes/comment.js
+++ routes/comment.js
@@ -1,10 +1,13 @@
 const { Router } = require('express');
 const {
   createNewComment,
+  deleteComment
 } = require('../controllers/comment');
 
 const commentRouter = Router();
 
 commentRouter.post('/', createNewComment);
+commentRouter.delete('/', deleteComment);
+
 
 module.exports = commentRouter;
