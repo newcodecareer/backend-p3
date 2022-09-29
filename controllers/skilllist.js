87b9c1e4c6b6ae6944233fd74d86fddc68be0a80
jeff <kuokamhung@yahoo.com.hk>
2022-09-29 18:00:36 +1000

    feat

--- controllers/skilllist.js
+++ controllers/skilllist.js
@@ -2,25 +2,61 @@ const { StatusCodes } = require('http-status-codes');
 const Skilllist = require('../models/Skilllist');
 
 const createNewSkilllist = async (req, res) => {
-    const { Listcategory, Listtitle, Listdescription, Listprice, Listdistance, Listavaliability } = req.body;
-    
-    try {
-      const NewSkilllist = await Skilllist({
-        Listcategory, 
-        Listtitle, 
-        Listdescription, 
-        Listprice, 
-        Listdistance, 
-        Listavaliability
+  const { Listcategory, Listtitle, Listdescription, Listprice, Listdistance, Listavaliability } =
+    req.body;
+
+  try {
+    const NewSkilllist = await Skilllist({
+      Listcategory,
+      Listtitle,
+      Listdescription,
+      Listprice,
+      Listdistance,
+      Listavaliability,
     }).save();
 
-      return res.status(StatusCodes.OK).json(NewSkilllist);
-    } catch (err) {
-      return res.status(StatusCodes.NOT_IMPLEMENTED).json(err);
-    }
-  };
-  
-  module.exports = {
-    createNewSkilllist,
-  };
-  
+    return res.status(StatusCodes.OK).json(NewSkilllist);
+  } catch (err) {
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json(err);
+  }
+};
+
+const getALLNewskills = async (req, res) => {
+  try {
+    const skilllist = await Skilllist.find();
+    res.status(200).json({
+      status: 'success',
+      data: {
+        skilllist,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+const getNewskills = async (req, res) => {
+  try {
+    const Skilllists = await Skilllist.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        Skilllists,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+module.exports = {
+  createNewSkilllist,
+  getALLNewskills,
+  getNewskills,
+};
--- routes/skilllist.js
+++ routes/skilllist.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
-const { createNewSkilllist } = require('../controllers/skilllist');
+const { createNewSkilllist, getALLNewskills, getNewskills } = require('../controllers/skilllist');
 
 const skilllistRouter = Router();
 
 skilllistRouter.post('/', createNewSkilllist);
+skilllistRouter.get('/', getALLNewskills);
+skilllistRouter.get('/:id', getNewskills);
 
 module.exports = skilllistRouter;
