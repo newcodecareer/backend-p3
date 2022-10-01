d8ac59be711897da8409f668d9f32546af960406
Alexhaoyu <110823364+Alex-H4O@users.noreply.github.com>
2022-10-01 18:26:25 +1000

    Revert "feat: read comment by id (#18)"
    This reverts commit acc3ee28f868cbb2d9168ac35c8d2f9fabbc53b6.

--- routes/comment.js
+++ routes/comment.js
@@ -1,10 +1,13 @@
 const { Router } = require('express');
-const { createNewComment, deleteCommentById, getCommentById } = require('../controllers/comment');
+const {
+  createNewComment,
+  deleteCommentById,
+} = require('../controllers/comment');
 
 const commentRouter = Router();
 
 commentRouter.post('/', createNewComment);
 commentRouter.delete('/:id', deleteCommentById);
-commentRouter.get('/:id', getCommentById);
+
 
 module.exports = commentRouter;
