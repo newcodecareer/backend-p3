2bbc964b93c27e38d40e347b59892c3a00c7e5b2
icegerb <88328231+icegerb@users.noreply.github.com>
2022-10-08 21:45:55 +1000

    fix: references and add 3 features (#26)
    * fix: references and add 3 features
    
    * fix: comment model
    
    * fix: some small updates

--- routes/comment.js
+++ routes/comment.js
@@ -1,10 +1,18 @@
 const { Router } = require('express');
-const { createNewComment, deleteCommentById, getCommentById } = require('../controllers/comment');
+const {
+  createNewComment,
+  deleteCommentById,
+  getCommentById,
+  getAllComments,
+  updateCommentById,
+} = require('../controllers/comment');
 
 const commentRouter = Router();
 
 commentRouter.post('/', createNewComment);
 commentRouter.delete('/:id', deleteCommentById);
 commentRouter.get('/:id', getCommentById);
+commentRouter.get('/', getAllComments);
+commentRouter.put('/:id', updateCommentById);
 
 module.exports = commentRouter;
--- routes/index.js
+++ routes/index.js
@@ -11,9 +11,8 @@ const v1Router = express.Router();
 v1Router.use('/users', userRouter);
 v1Router.use('/customers', customerRouter);
 v1Router.use('/auth', authRouter);
-v1Router.use('/skillLists', skillListRouter);
+v1Router.use('/skill-lists', skillListRouter);
 v1Router.use('/posts', postRouter);
 v1Router.use('/comments', commentRouter);
-
 
 module.exports = v1Router;
