41d58244886cacdfaee7b28bff7feb6831deddc1
EricaP451 <111969272+EricaP451@users.noreply.github.com>
2022-11-23 13:04:06 +1100

    feat: skill List swagger (#38)

--- routes/skillList.js
+++ routes/skillList.js
@@ -9,10 +9,120 @@ const {
 
 const skillListRouter = Router();
 
+/**
+ * @swagger
+ *  components:
+ *    schemas:
+ *      SkillList:
+ *        type: object
+ *        properties:
+ *          category:
+ *            type: string
+ *          title:
+ *            type: string
+ *          description:
+ *            type: string
+ *          price:
+ *            type: number
+ *          distance:
+ *            type: number
+ *          location:
+ *            type: string
+ *          availability:
+ *            type: string
+ */
+
+/**
+ * @swagger
+ *  /v1/skill-lists:
+ *    post:
+ *      summary: Create a new SkillList
+ *      tags: [SkillLists]
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/SkillList'
+ *      responses:
+ *        "200":
+ *          description: Post new skillList success
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/SkillList'
+ */
 skillListRouter.post('/', createNewSkillList);
+
+/**
+ * @swagger
+ * /v1/skill-lists:
+ *  get:
+ *   summary: Get all SkillLists
+ *   tags:
+ *      - SkillLists
+ *   description: SkillLists
+ *   content:
+ *      application/json:
+ *   responses:
+ *    '200':
+ *      description: Get all SkillLists
+ */
 skillListRouter.get('/', getAllSkillLists);
+
+/**
+ * @swagger
+ *  /v1/skill-lists/{id}:
+ *    get:
+ *      summary: Show a SkillList
+ *      tags: [SkillLists]
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *      responses:
+ *        "200":
+ *          description: Show SkillList success
+ */
 skillListRouter.get('/:id', getSkillListById);
+
+/**
+ * @swagger
+ *  /v1/skill-lists/{id}:
+ *    delete:
+ *      summary: Delete a SkillList
+ *      tags: [SkillLists]
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *      responses:
+ *        "200":
+ *          description: Delete SkillList success
+ */
 skillListRouter.delete('/:id', deleteSkillListById);
+
+/**
+ * @swagger
+ *  /v1/skill-lists/{id}:
+ *    put:
+ *      summary: Update a SkillList
+ *      tags: [SkillLists]
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/SkillList'
+ *      responses:
+ *        "200":
+ *          description: Update SkillList success
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/SkillList'
+ */
 skillListRouter.put('/:id', updateSkillListById);
 
 module.exports = skillListRouter;
