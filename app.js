cf92b67f93a15f8ee63f26e4f20c5773467052c1
newcodecareer <815773677@qq.com>
2022-11-24 18:59:50 +1000

    fix: db files, add logic method for db connection, show connection status

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ const port = process.env.PORT || 3000;
 const start = async () => {
   try {
     // await connectDB(process.env.LOCAL_STRING);
-    await connectDB(process.env.MONGO_URI);
+    await connectDB( );
     app.listen(port, () => logger.info(`Server is listening on port ${port}...`));
   } catch (error) {
     console.log(error);
--- db/connect.js
+++ db/connect.js
@@ -1,5 +1,26 @@
 const mongoose = require('mongoose');
+const logger = require('../utils/logger');
 
-const connectDB = (url) => mongoose.connect(url);
+const connectDB = () => {
+  const connectionString = process.env.MONGO_URI;
+  if (!connectionString) {
+    logger.info('connection string is undefined');
+
+    process.exit(1);
+  }
+  const db = mongoose.connection;
+  db.on('connected', () => {
+    // logger.info
+    logger.info(`DB connected, ${connectionString}`);
+  });
+  db.on('error', (error) => {
+    logger.info(error);
+    process.exit(2);
+  });
+  db.on('disconnected', () => {
+    logger.info('db disconnected');
+  });
+  return mongoose.connect(connectionString);
+};
 
 module.exports = connectDB;
